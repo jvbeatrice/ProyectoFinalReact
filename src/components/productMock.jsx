807323d80f3f49739fc79d1dd557d1e678fc6c3b
jvbeatrice <julietabeatrice@gmail.com>
2023-06-06 15:34:11 -0300

    Added products for each category

--- src/components/productMock.jsx
+++ src/components/productMock.jsx
@@ -53,4 +53,31 @@ export const products = [
     category: "pastas",
     img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/434/490/products/fideos-integral-tirabuzon-grun1-293d4c76af56bc222616397667553298-640-0.webp",
   },
+  {
+    title: "Aceite coco spray Chia Graal",
+    price: 2500,
+    stock: 3,
+    description:
+      "Aceite de coco virgen para rociar sobre sarten y o comidas, un aceite más ligero, más natural y menos procesado.",
+    category: "aceites",
+    img: "https://d2r9epyceweg5n.cloudfront.net/stores/001/437/443/products/aceite-coco-spray1-6706d58fb89c484f8016670577717807-640-0.png",
+  },
+  {
+    title: "Porotos pallares",
+    price: 1300,
+    stock: 12,
+    description:
+      "Paquete de 1kg. de porotos pallares, los más grandes y ricos que vas a encontrar entre nuestras legumbres.",
+    category: "legumbres",
+    img: "https://legumbreselio.com/wp-content/uploads/porotos-pallares-1200x800.jpg",
+  },
+  {
+    title: "Dentífrico SriSri menta",
+    price: 1300,
+    stock: 5,
+    description:
+      "Pasta dental sin flúor ni sulfatos, cuidado consciente y natural de tus dientes. Sabor menta fresca.",
+    category: "cuidadopersonal",
+    img: "https://darvitasa.com/wp-content/uploads/2021/11/D_NQ_NP_857881-MLA48328817451_112021-O.webp",
+  },
 ];
